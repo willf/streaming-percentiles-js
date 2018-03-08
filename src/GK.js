524497c9fd06d203aaafba884299f84f38f3959b
Steven Engelhardt <sengelha@gmail.com>
2018-03-07 23:33:10 -0600

    Fix _construct_band_lookup; make static method on class

--- src/GK.js
+++ src/GK.js
@@ -28,11 +28,11 @@ export default class GK {
 
   _compress() {
     var two_epsilon_n = 2 * this.epsilon * this.n;
-    var bands = this._construct_band_lookup(two_epsilon_n);
+    var bands = GK._construct_band_lookup(two_epsilon_n);
     // We must always keep the first & last nodes as those
     // are global min/max
     for (var i = this.S.length - 2; i >= 1; --i) {
-      var g_i_star = this.S[i].g; // TODO
+      var g_i_star = this.S[i].g; // TODO: This is not correct.
       if (bands[this.S[i].delta] <= bands[this.S[i+1].delta] &&
         (g_i_star + this.S[i+1].g + this.S[i+1].delta) < two_epsilon_n) {
         this._delete_tuple(i);
@@ -40,13 +40,14 @@ export default class GK {
     }
   }
 
-  _construct_band_lookup(two_epsilon_n) {
+  static _construct_band_lookup(two_epsilon_n) {
     var bands = Array(two_epsilon_n + 1);
     bands[0] = -1; // delta = 0 is its own band
     bands[two_epsilon_n] = 0; // delta = two_epsilon_n is band 0 by definition
 
     var p = Math.floor(two_epsilon_n);
-    for (var alpha = 1; alpha <= Math.ceil(Math.log(two_epsilon_n)); ++alpha) {
+    // Need to go from [1, ceil(log(2en)) + 1] to handle 2en = 16 cases (among others)
+    for (var alpha = 1; alpha <= Math.ceil(Math.log(two_epsilon_n)) + 1; ++alpha) {
         var two_alpha_minus_1 = Math.pow(2, alpha-1);
         var two_alpha = Math.pow(2, alpha);
         var lower = p - two_alpha - (p % two_alpha);
@@ -58,19 +59,12 @@ export default class GK {
         }
     }
     
-    console.log("Bands = " + JSON.stringify(bands));
     return bands;
   }
 
   _delete_tuple(i) {
-    console.log("Deleting tuple at index " + i);
-    if (i == 0 || i == this.S.length - 1)
-      throw "Cannot delete these tuples!";
-
     var merged = {v: this.S[i+1].v, g: this.S[i].g + this.S[i+1].g, delta: this.S[i+1].delta};
-    //var preS = this.S.slice();
     this.S.splice(i, 2, merged);
-    //console.log("S before = " + JSON.stringify(preS) + " after = " + JSON.stringify(this.S));
   }
 
   _do_insert(v) {
@@ -83,7 +77,6 @@ export default class GK {
     else
       delta = Math.floor(2 * this.epsilon * this.n) - 1;
     var tuple = {v: v, g: 1, delta: delta};
-    //console.log("inserting " + JSON.stringify(tuple) + " at index " + i);
     this.S.splice(i, 0, tuple);
   }
 
--- test/GK-test.js
+++ test/GK-test.js
@@ -1,9 +1,10 @@
 var tape = require("tape"),
-    streamingPercentiles = require("../");
+    sp = require("../");
 
+// TODO: Include enough insertions to require compress
 tape("GK e=0.1 inner state", function(test) {
     var seq = [11, 20, 18, 5, 12, 6, 3, 2];
-    var gk = new streamingPercentiles.GK(0.1);
+    var gk = new sp.GK(0.1);
     for (var i = 0; i < seq.length; ++i)
         gk.insert(seq[i]);
     test.equal(gk.n, seq.length);
@@ -24,11 +25,34 @@ tape("GK e=0.1 stress", function(test) {
     var arr = [];
     for (var i = 0; i < 1000; ++i)
         arr.push(Math.random());
-    var gk = new streamingPercentiles.GK(0.1);
+    var gk = new sp.GK(0.1);
     for (var i = 0; i < arr.length; ++i) {
         gk.insert(arr[i]);
         var q = gk.quantile(0.5);
         console.log(q);
     }
+    test.end();
+});
+
+tape("GK._construct_band_lookup", function(test) {
+    test.deepEquals(sp.GK._construct_band_lookup(0), [0]);
+    test.deepEquals(sp.GK._construct_band_lookup(1), [-1, 0]);
+    test.deepEquals(sp.GK._construct_band_lookup(2), [-1, 1, 0]);
+    test.deepEquals(sp.GK._construct_band_lookup(3), [-1, 1, 1, 0]);
+    test.deepEquals(sp.GK._construct_band_lookup(4), [-1, 2, 2, 1, 0]);
+    test.deepEquals(sp.GK._construct_band_lookup(5), [-1, 2, 2, 1, 1, 0]);
+    test.deepEquals(sp.GK._construct_band_lookup(6), [-1, 2, 2, 2, 2, 1, 0]);
+    test.deepEquals(sp.GK._construct_band_lookup(7), [-1, 2, 2, 2, 2, 1, 1, 0]);
+    test.deepEquals(sp.GK._construct_band_lookup(8), [-1, 3, 3, 3, 3, 2, 2, 1, 0]);
+    test.deepEquals(sp.GK._construct_band_lookup(9), [-1, 3, 3, 3, 3, 2, 2, 1, 1, 0]);
+    test.deepEquals(sp.GK._construct_band_lookup(10), [-1, 3, 3, 3, 3, 2, 2, 2, 2, 1, 0]);
+    test.deepEquals(sp.GK._construct_band_lookup(11), [-1, 3, 3, 3, 3, 2, 2, 2, 2, 1, 1, 0]);
+    test.deepEquals(sp.GK._construct_band_lookup(12), [-1, 3, 3, 3, 3, 3, 3, 3, 3, 2, 2, 1, 0]);
+    test.deepEquals(sp.GK._construct_band_lookup(13), [-1, 3, 3, 3, 3, 3, 3, 3, 3, 2, 2, 1, 1, 0]);
+    test.deepEquals(sp.GK._construct_band_lookup(14), [-1, 3, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 1, 0]);
+    test.deepEquals(sp.GK._construct_band_lookup(15), [-1, 3, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 1, 1, 0]);
+    test.deepEquals(sp.GK._construct_band_lookup(16), [-1, 4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 2, 2, 1, 0]);
+    test.deepEquals(sp.GK._construct_band_lookup(24), [-1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 2, 2, 1, 0]);
+    test.deepEquals(sp.GK._construct_band_lookup(25), [-1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 2, 2, 1, 1, 0]);
     test.end();
 })
